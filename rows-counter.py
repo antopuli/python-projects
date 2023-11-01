d59d9c6729dbe24e44d26d2b8e00812279cdf217
antopuli <anto.pul96@gmail.com>
2023-11-01 15:50:17 +0100

    Update rows-counter.py
    I created a function from the previously program that take a text as argument and perform the task of count the text's rows.

--- rows-counter.py
+++ rows-counter.py
@@ -1,6 +1,44 @@
 
-# TESTO A CUI CONTARE LE RIGHE
-text = """Nel mezzo del cammin di nostra vita
+# defining 'count_rows' function with a parameter named 'text'
+def count_rows(text):
+    
+    # create a list containing all the characters of the given argument
+    tList = list(text) 
+    
+    # create a list that will contain all the text's rows
+    rows = []
+    
+    # create a string that will contain a row to append to rows
+    row = ''
+
+    # the for loop navigate from 0 to tList's length
+    
+    # if the element of tList with a index of 'i' is equal to '\n' or 
+    # 'i' is the last index of tList then the 'row' variable will be append to
+    # the 'rows' one as a list and it will be initialized as an empty string
+    
+    # else the element of tList with a index of 'i' will be concatenated to
+    # the 'row' existing string
+    
+    for i in range(len(tList)):
+        if tList[i] == '\n' or i == (len(tList)-1):
+            rows.append(list(row))
+            row = ''
+        else:
+            row += tList[i]
+
+    # the for loop navigate from 0 to the number of empty lists contained in the
+    # 'rows' variable
+    
+    # remove from the 'rows' list the empyt ones
+    
+    for i in range(rows.count([])): rows.remove([])
+    
+    # return the number of rows
+    return len(rows)
+    
+
+dante = """Nel mezzo del cammin di nostra vita
 mi ritrovai per una selva oscura, 
 che la diritta via era smarrita. 
 
@@ -8,23 +46,5 @@ Ahi quanto a dir qual era è cosa dura,
 esta selva selvaggia e aspra e forte, 
 che nel pensier rinnova la parola."""
 
-# DEFINISCO LE STRUTTURE DATI DI SUPPORTO
-tList = list(text)
-rows = []
-supporingList = ''
 
-# NAVIGO ALL'INTERNO DELLE LETTERE DEL TESTO
-# ED INSERISCO ALL'INTERNO DI UNA LISTA LE RIGHE
-for i in range(len(tList)):
-    if tList[i] == '\n' or i == (len(tList)-1):
-        rows.append(list(supporingList))
-        supporingList = ''
-    else:
-        supporingList += tList[i]
-
-# RIMUOVO DALLA LISTA LE SUB-LISTE VUOTE, OVVERO LE
-# RIGHE PRIVE DI TESTO
-for i in range(rows.count([])): rows.remove([])
-
-# STAMPO A SCHERMO IL RISULTATO DEL PROGRAMMA
-print(f'Il testo contiene {len(rows)} righe.')
+print(f'Text contains {count_rows(dante)} rows.')
